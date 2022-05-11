c87947043c9502ef4b2e830a0f096192196dae1a
Leidysz <leiidiane.aguiar@gmail.com>
2022-05-11 09:44:53 -0300

    Subtotal & Total methods

--- src/main/java/com/educandoweb/course/entities/Order.java
+++ src/main/java/com/educandoweb/course/entities/Order.java
@@ -99,6 +99,14 @@ public class Order implements Serializable {
 	public Set<OrderItem> getItems() {
 		return items;
 	}
+	
+	public Double getTotal() {
+		double sum = 0.0;
+		for (OrderItem xItem : items) {
+			sum += xItem.getSubTotal();
+		}
+		return sum;
+	}
 
 	@Override
 	public int hashCode() {
